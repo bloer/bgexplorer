232e438a2f3b00c53712497f1f460265d4c03a37
Ben Loer <ben.loer@pnnl.gov>
2017-08-16 10:38:50 -0700

    add modeleditor directory

---
+++ bgexplorer/modeleditor/modeleditor.py
@@ -0,0 +1,8 @@
+#python 2/3 compatibility
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
+from flask import Blueprint, render_template
+
+
