666643b4130bcce3f9f09e304ab46eeba95aee9d
Ben Loer <ben.loer@pnnl.gov>
2020-07-08 13:19:44 -0700

    enable upload json sim db entries

--- bgexplorer/simsviewer/simsviewer.py
+++ bgexplorer/simsviewer/simsviewer.py
@@ -1,9 +1,13 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from flask import (Blueprint, render_template, render_template_string,
-                   request, abort, url_for, g, json,
-                   Response)
+                   request, abort, url_for, g, json, flash, redirect,
+                   Response, get_flashed_messages)
+
 from .. import utils
+import gzip
+import zipfile
+import tarfile
 #import json
 
 
@@ -28,7 +32,7 @@ def findsimmatches(dataset, model=None):
                     res.append(match)
             except TypeError:
                 pass
-    return res   
+    return res
 
 
 class SimsViewer(object):
@@ -40,15 +44,18 @@ class SimsViewer(object):
         summarypro: a projection operator to generate flat summary tables
         summarycolumns (list): list of column names for summary page
         detailtemplate: path to template file for detailed simulation view
+        enableupload (bool): if True, allow uploading new entries
     """
     def __init__(self, app=None, simsdb=None, url_prefix='/simulations',
-                 summarypro=None, summarycolumns=None, detailtemplate=None):
+                 summarypro=None, summarycolumns=None, detailtemplate=None,
+                 enableupload=True):
         self.app = app
         self._simsdb = simsdb
         self.summarypro = summarypro
         self.summarycolumns = summarycolumns
+        self.enable_upload = enableupload
 
-        self.bp = Blueprint('simsviewer', __name__, 
+        self.bp = Blueprint('simsviewer', __name__,
                             static_folder='static',
                             template_folder='templates')
         self.bp.add_app_template_global(lambda : self, 'getsimsviewer')
@@ -71,26 +78,40 @@ class SimsViewer(object):
                     request.args = args
                 except (KeyError, json._json.JSONDecodeError):
                     pass
-        
+
         self.register_endpoints()
         if self.app:
             self.init_app(app, url_prefix)
 
     def init_app(self, app, url_prefix=''):
         """Register ourselves with the app"""
-        app.register_blueprint(self.bp, 
+        app.register_blueprint(self.bp,
                                url_prefix=url_prefix)
         app.extensions['SimulationsViewer'] = self
         key = 'SIMULATIONSVIEWER_SUMMARY_PROJECTION'
         self.summarypro = app.config.setdefault(key, self.summarypro)
         key = 'SIMULATIONSVIEWER_SUMMARY_COLUMNS'
         self.summarycolumns = app.config.setdefault(key, self.summarycolumns)
-        
+        key = "ENABLE_SIMULATION_UPLOADS"
+        self.enable_upload = app.config.setdefault(key, self.enable_upload)
+
 
     @property
     def simsdb(self):
         return self._simsdb or utils.get_simsdb()
-    
+
+    def getcolnames(self, sims):
+        """ Get the column names to display in summary table """
+        columns = self.summarycolumns or []
+        if sims and not columns:
+            #non-underscore keys with string or number values
+            for key, val in sims[0].items():
+                if (not key.startswith('_')
+                    and key != 'id'
+                    and isinstance(val,(str,int,float))):
+                    columns.append(key)
+        return columns
+
     def register_endpoints(self):
         """Attach the model if requested"""
         @self.bp.url_value_preprocessor
@@ -104,31 +125,97 @@ class SimsViewer(object):
         def overview():
             query = request.args.get('query',None)
             try:
-                sims = list(self.simsdb.runquery(query, 
+                sims = list(self.simsdb.runquery(query,
                                                 projection=self.summarypro))
             except Exception as e:
                 abort(400, "Invalid query specifier")
-            columns = self.summarycolumns or []
-            if sims and not columns:
-                #non-underscore keys with string or number values
-                for key, val in sims[0].items():
-                    if (not key.startswith('_') 
-                        and key != 'id'
-                        and isinstance(val,(str,int,float))):
-                        columns.append(key)
+            columns = self.getcolnames(sims)
             return render_template('simoverview.html', sims=sims, query=query,
                                    colnames=columns)
-        
+
         @self.bp.route('/dataset/<dataset>')
         def detailview(dataset):
             detail = self.simsdb.getdatasetdetails(dataset)
             matches = findsimmatches(dataset)
             return render_template('datasetview.html',dataset=dataset,
                                    detail=detail)
-            
+
 
         @self.bp.route('/dataset/<dataset>/raw')
         def rawview(dataset):
             """Export the dataset as raw JSON"""
             detail = self.simsdb.getdatasetdetails(dataset)
             return json.jsonify(detail)
+
+        if not self.enable_upload:
+            return
+
+        @self.bp.route('/upload', methods=('GET','POST'))
+        def upload():
+            """ Upload new JSON-formatted entries """
+            if request.method == 'POST':
+                flist = request.files.getlist('fupload')
+                newentries = sum((self.parseupload(f) for f in flist), [])
+                if newentries:
+                    colnames = self.getcolnames(newentries)
+                    return render_template('simoverview.html', sims=newentries,
+                                           colnames=colnames, temp=True)
+                else:
+                    flash("No valid entries in uploaded files",'error')
+            return render_template('uploadsimdata.html')
+
+        @self.bp.route('/confirmupload', methods=('POST',))
+        def confirmupload():
+            """ Confirm uploads with form """
+            confirmed = []
+            for key, val in request.form.items():
+                if key.startswith('simdata'):
+                    num = key[7:]
+                    confirm = request.form.get('confirm'+num,'off') == 'on'
+                    if confirm:
+                        confirmed.append(val)
+            numinserted = 0
+            for entry in confirmed:
+                try:
+                    self.simsdb.addentry(entry, fmt='json')
+                    numinserted += 1
+                except Exception as e:
+                    flash("Database error occurred: %s"%e, 'error')
+            flash("%d sim data entries successfully added"%numinserted, 'info')
+            return redirect(url_for('.overview'))
+
+    def parseupload(self, afile):
+        files = [afile]
+        # see if it's a zip or gzip archive
+        try:
+            tf = tarfile.open(fileobj=afile)
+            files = [tf.extractfile(name) for name in tf.getnames()]
+            for f,name in zip(files, tf.getnames()):
+                f.filename = name
+        except tarfile.TarError:
+            afile.seek(0)
+            pass
+        try:
+            zf = zipfile.ZipFile(afile)
+            files = [zf.open(name) for name in zf.namelist()]
+        except zipfile.BadZipFile:
+            afile.seek(0)
+            pass
+
+        def _load(jfile):
+            with gzip.open(jfile) as decomp:
+                result = None
+                try:
+                    result = json.load(decomp)
+                except OSError:
+                    jfile.seek(0)
+                    result = json.load(jfile)
+                except (json._json.JSONDecodeError) as e:
+                    flash("Error decoding JSON: %s"%e, 'error')
+            try:
+                result['_filename'] = getattr(jfile, 'filename', jfile.name)
+            except (AttributeError, TypeError):
+                pass
+            return result
+        return list(filter(lambda x: x is not None, map(_load, files)))
+
