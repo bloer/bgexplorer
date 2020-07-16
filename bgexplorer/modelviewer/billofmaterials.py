567f6726683d2c680b9899545dc583d9af824818
Ben Loer <ben.loer@pnnl.gov>
2020-07-16 16:02:03 -0700

    protect the rate conversion in a try/catch block

--- bgexplorer/modelviewer/billofmaterials.py
+++ bgexplorer/modelviewer/billofmaterials.py
@@ -2,6 +2,7 @@
 from flask import g
 from collections import namedtuple, OrderedDict
 from bgmodelbuilder.utilities import Isotope
+from bgmodelbuilder import units
 
 BOMRow = namedtuple('BOMRow',('outline','path','component',
                               'weight','totalweight'))
@@ -15,8 +16,8 @@ def getbomrows(row=None, form="%02d"):
                      component=g.model.assemblyroot,
                      weight=1,
                      totalweight=1)
-    
-    myrows = [row] 
+
+    myrows = [row]
 
     parent = row.component
     subs = parent.getcomponents(deep=False, withweight=True)
@@ -32,40 +33,40 @@ def getbomrows(row=None, form="%02d"):
                               totalweight=row.totalweight*weight)
             myrows.extend(getbomrows(childrow, form=form))
     return myrows
-                              
+
 
 def getdefaultcols():
     """Return an OrderedDict listing columns to show in the bill of materials
     table. Keys are the column headings, and values are functions
-    that take a BOMRow as argument. 
+    that take a BOMRow as argument.
 
-    Users extending the bill of materials table should most often extend 
-    this list rather than replacing it. If styling is required, 
-    results should be wrapped in a <span> element. 
+    Users extending the bill of materials table should most often extend
+    this list rather than replacing it. If styling is required,
+    results should be wrapped in a <span> element.
     """
-    
+
     def makelink(url, text, emptylinktext='link'):
-        """Embed 'text' in a hyperlink if url is not empty, else return 
+        """Embed 'text' in a hyperlink if url is not empty, else return
         plain text.  If url is not empty but text is, use empylinktext instead
         """
         if url:
-            return '<a href="{}">{}</a>'.format(url, 
+            return '<a href="{}">{}</a>'.format(url,
                                                 text if text else emptylinktext)
         else:
             return text
-            
+
     def partnum(row):
         datasheet = row.component.moreinfo.get('datasheet',None)
         partnum = row.component.moreinfo.get('partnum','')
         return makelink(datasheet, partnum, 'datasheet')
-            
+
     def assayref(row):
         return ' '.join(
             makelink(spec.moreinfo.get('url'),
-                     spec.moreinfo.get('reference',''), 
+                     spec.moreinfo.get('reference',''),
                      'ref')
             for spec in row.component.getspecs())
-        
+
     def assaydetail(row):
         return ' '.join(spec.moreinfo.get('refdetail','') + " " + spec.comment
                         for spec in row.component.getspecs())
@@ -80,7 +81,10 @@ def getdefaultcols():
                     return ''
                 rate = sum(spec.ratewitherr for spec in specmatch)
                 if rate and unit:
-                    rate = rate.to(unit).m
+                    try:
+                        rate = rate.to(unit).m
+                    except units.errors.DimensionalityError:
+                        pass
                 if all(spec.islimit for spec in specmatch):
                     return '<'+str(rate)
                 return rate
@@ -88,21 +92,21 @@ def getdefaultcols():
                 return "err"
 
         return _getisorate
-            
+
 
     return OrderedDict((
         ('Weight', lambda r: r.weight),
         ('Description', lambda r: r.component.description),
         ('Comment', lambda r: r.component.moreinfo.get('comment','')),
         ('Partnum', partnum),
-        ('Material', lambda r: r.component.material), 
+        ('Material', lambda r: r.component.material),
         ('Vendor', lambda r: r.component.moreinfo.get('vendor','')),
-        ('Mass', lambda r:r.component.mass ), 
+        ('Mass', lambda r:r.component.mass ),
         ('Assay Ref', assayref),
         #('Assay Detail', assaydetail),
         ('U238 [mBq/kg]', isorate('U238','mBq/kg')),
         ('Th232 [mBq/kg]', isorate('Th232', 'mBq/kg')),
         ('K40 [mBq/kg]', isorate('K40','mBq/kg')),
-        
-        
+
+
     ))
