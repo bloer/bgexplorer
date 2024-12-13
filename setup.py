b51ecf175abaaeb5016cf16d5f030987b6dfc3c2
Ben Loer <ben.loer@pnnl.gov>
2024-12-13 12:46:11 -0800

    Update setup.py, freeze werkzeug compatible for flask2.0

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(name='bgexplorer',
       zip_safe=False,
       install_requires=[
           'flask>=2.0,<2.1',
+          'werkzeug==2.2.2',
           'wtforms<3.0',
           'flask-wtf',
           'flask-bootstrap',
